eb7ac3ec7356c51874077c5b5d8befbb40708631
Rawand Dargali <rdargali@yahoo.com>
2020-11-28 02:15:11 -0600

    start open timer working with clicks and new dom manip

--- app.js
+++ app.js
@@ -1,42 +1,72 @@
-let seconds = 00
-let tens = 00
+let openSecondsCount = 00
+let openTensCount = 00
+let openClicksCount = 0
 
-const appendTens = document.getElementById("tens")
-const appendSeconds = document.getElementById("seconds")
+let closedSecondsCount = 00
+let closedTensCount = 00
+let closedClicksCount = 0
+
+const openTens = document.getElementById("open-tens")
+const openSeconds = document.getElementById("open-seconds")
+const openClicks = document.getElementById("open-clicks")
+
+const closedTens = document.getElementById("closed-tens")
+const closedSeconds = document.getElementById("closed-seconds")
+const closedClicks = document.getElementById("closed-clicks")
 
 const buttonOpen = document.getElementById("button-open")
 const buttonClose = document.getElementById("button-close")
-const buttonReset = document.getElementById("button-Reset")
+const buttonReset = document.getElementById("button-reset")
 
-let interval;
+let openInterval;
+let closedInterval;
 
 //open
 
 function startOpenTimer() {
-    tens++
+    openTensCount++
+    
+    openClicks.innerHTML = openClicksCount
 
-    if(tens < 9) {
-        appendTens.innerHTML = "0" + tens
+    if(openTensCount < 9) {
+        openTens.innerHTML = "0" + openTensCount
     }
 
-    if(tens > 9) {
-        appendTens.innerHTML = tens
+    if(openTensCount > 9) {
+        openTens.innerHTML = openTensCount
     }
 
-    if(tens > 99){
-        seconds++
-        appendSeconds.innerHTML = "0" + seconds
-        tens = 0
-        appendTens.innerHTML = "0" + 0
+    if(openTensCount > 99){
+        openSecondsCount++
+        openSeconds.innerHTML = "0" + openSecondsCount
+        openTensCount = 0
+        openTens.innerHTML = "0" + 0
     }
 
-    if(seconds > 9){
-        appendSeconds.innerHTML = seconds
+    if(openSecondsCount > 9){
+        openSeconds.innerHTML = openSecondsCount
     }
 }
 
-buttonOpen.addEventListener("click", function() {
-   
-    interval = setInterval(startOpenTimer)
 
+
+buttonOpen.addEventListener("click", function() {
+    openClicksCount++
+    openInterval = setInterval(startOpenTimer)
+
+})
+
+buttonClose.addEventListener("click", function() {
+    console.log("hello close")
+    closedClicksCount++
+    clearInterval(openInterval)
+})
+
+buttonReset.addEventListener("click", function() {
+    console.log("hello")
+    clearInterval(startOpenTimer)
+    // tens = "00"
+    // seconds = "00"
+    // appendSeconds.innerHTML = seconds
+    // appendTens.innerHTML = tens
 })
