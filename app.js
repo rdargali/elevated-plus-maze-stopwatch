585f93e88437d80ad9468f9ab8b6842cd3af0778
Rawand Dargali <rdargali@yahoo.com>
2020-11-28 00:27:44 -0600

    open timer function

---
+++ app.js
@@ -0,0 +1,41 @@
+const seconds = 00
+const tens = 00
+
+const appendTens = document.getElementById("tens")
+const appendSeconds = document.getElementById("seconds")
+
+const buttonOpen = document.getElementById("button-open")
+const buttonClose = document.getElementById("button-close")
+const buttonReset = document.getElementById("button-Reset")
+
+const Interval
+
+//open
+
+function startOpenTimer() {
+    tens++
+
+    if(tens < 9) {
+        appendTens.innerHTML = "0" + tens
+    }
+
+    if(tens > 9) {
+        appendTens.innerHTML = tens
+    }
+
+    if(tens > 99){
+        seconds++
+        appendSeconds.innerHTML = "0" + seconds
+        tens = 0
+        appendTens.innerHTML = "0" + 0
+    }
+
+    if(seconds > 9){
+        appendSeconds.innerHTML = seconds
+    }
+}
+
+buttonOpen.addEventListener("click", function() {
+    interval = setInterval(startOpenTimer)
+
+})
